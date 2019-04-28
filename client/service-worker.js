ec8deb437127d8910d7355b6cf9e023616766fae
Hektor Wallin <hektorw@gmail.com>
2019-04-28 20:09:21 +0200

    Activate sw direct

--- client/service-worker.js
+++ client/service-worker.js
@@ -8,6 +8,8 @@ const precacheFiles = [
 ]
 
 self.addEventListener('install', event => {
+  self.skipWaiting()
+
   event.waitUntil(
     caches.open(cacheName).then(cache => cache.addAll(precacheFiles))
   )
